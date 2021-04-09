71528c934baf855679722d5dfc8bedda89e61a11
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-04-09 12:10:55 +0100

    CSS changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </div>
     </div>
   );
 }
