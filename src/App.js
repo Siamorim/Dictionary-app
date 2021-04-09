d03878e7c7a67d9ddc4d74e7b1b15a9878e7189b
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-04-09 16:06:02 +0100

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">Coded by Sílvia Amorim</footer>
+        </small>
       </div>
     </div>
   );
