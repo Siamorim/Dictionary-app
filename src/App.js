430df5b2b167e06ff02b159b6c7cfe91c10f6101
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-04-12 23:19:33 +0100

    CSS changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary.js";
+import logo from "./booksmall.png";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </div>
-        <main>
-          <Dictionary />
-        </main>
-        <small>
-          <footer className="App-footer">Coded by Sílvia Amorim</footer>
-        </small>
+        <img src={logo} alt="Woman and books" className="logo" />
+        <div className="App-header">Dictionary</div>
+        <Dictionary />
       </div>
+      <footer className="code">
+        <span>This project was coded by </span>
+        <a
+          href="https://www.linkedin.com/in/s%C3%ADlvia-amorim-6220a053/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Sílvia Amorim
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Siamorim/Dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://elegant-visvesvaraya-fa6260.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
