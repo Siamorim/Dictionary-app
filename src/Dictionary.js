5a702675d708d8def62d229e54f7bfde3c3ff460
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-04-12 13:40:15 +0100

    Change API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,11 +13,10 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
-
-  //documentation: https://dictionaryapi.dev/
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
