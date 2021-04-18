36ca03f26d4a26a29b87db52b41be048367cc6be
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-04-18 23:36:03 +0100

    CSS

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,6 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
+        <span className="similar">Similar:</span>{" "}
         {props.synonyms.map(function (synonyms, index) {
           return <li key={index}>{synonyms}</li>;
         })}
